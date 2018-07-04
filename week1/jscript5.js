b8879cb3f3a35939d25370327988d31652cd39e4
karamGhazzi <Karam-g1993@hotmail.com>
2018-07-05 01:51:01 +0200

    integer numbers

---
+++ week1/jscript5.js
@@ -0,0 +1,22 @@
+>
+var z = 7.25;
+undefined
+    >
+    console.log(z);
+7.25
+undefined
+    >
+    var a = Math.floor(z);
+undefined
+    >
+    console.log(a);
+7
+undefined
+    >
+    var max = Math.max(z, a);
+undefined
+    >
+    console.log(max);
+7.25
+undefined
+    >
