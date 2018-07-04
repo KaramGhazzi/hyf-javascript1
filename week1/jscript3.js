abe61ce8469d9588b75e29d305342a75f9aef656
karamGhazzi <Karam-g1993@hotmail.com>
2018-07-05 01:28:01 +0200

    value of X

---
+++ week1/jscript3.js
@@ -0,0 +1,7 @@
+var x;
+console.log('the value of my variable x will be: undefined because i didnt define the value of X yet');
+console.log(x);
+x = 100;
+//giving value to X
+console.log('the value of x is: 100');
+console.log(x);
