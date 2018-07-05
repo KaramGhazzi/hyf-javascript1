cda507eea98ba2be8d9f2c46c71233d406b69a59
karamGhazzi <Karam-g1993@hotmail.com>
2018-07-05 02:18:37 +0200

    let String

---
+++ week1/jscript7.js
@@ -0,0 +1,15 @@
+>
+let myString = "this is a test";
+undefined
+    >
+    console.log(myString);
+this is a test
+undefined
+    >
+    myString.length;
+14
+    >
+    console.log(myString.length)
+14
+undefined
+    >
