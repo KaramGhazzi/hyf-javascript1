cf829006dfab73b331d4216f20d9458b22ca0c04
karamGhazzi <Karam-g1993@hotmail.com>
2018-07-05 02:07:46 +0200

    my array

---
+++ week1/jscript6.js
@@ -0,0 +1,21 @@
+>
+var anarray = [];
+undefined
+    >
+    console.log(anarray);
+[]
+undefined
+    >
+    var mypets = ["cat", "parrot", "sparrow"];
+undefined
+    >
+    console.log(mypets);
+['cat', 'parrot', 'sparrow']
+undefined
+    >
+    mypets.push("baby pig");
+4
+    >
+    console.log(mypets)['cat', 'parrot', 'sparrow', 'baby pig']
+undefined
+    >
