561d4ab221be134a6213767e9a94bca8b79f9881
karamGhazzi <Karam-g1993@hotmail.com>
2018-07-05 03:33:16 +0200

    step 10

---
+++ week1/jscript10.js
@@ -0,0 +1,14 @@
+>
+var myArray = [1, 'string', true, [], {}];
+undefined
+    >
+    console.log(myArray);
+[1, 'string', true, [], {}]
+undefined
+
+
+    >
+    console.log(6 / 0 === 10 / 0)
+true
+undefined
+    >
