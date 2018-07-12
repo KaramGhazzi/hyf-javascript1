7ecd92bf9515455fe478885a9a30f500657c05cd
karamGhazzi <Karam-g1993@hotmail.com>
2018-07-13 00:07:13 +0200

    strings!

---
+++ week2/strings.js
@@ -0,0 +1,7 @@
+let myString = "hello,this,is,a,difficult,to,read,sentence";
+console.log(myString);
+console.log(`the length is ${myString.length}`) or > console.log(myString.length);
+a = myString.replace(/,/g, " ")
+'hello this is a difficult to read sentence'
+console.log(a);
+hello this is a difficult to read sentence
